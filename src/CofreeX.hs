58040f76c7a8b623545e2ae0285f9cb9c2d98417
Parker Liu <liuchangsheng30@gmail.com>
2020-02-23 09:14:10 +0800

    Add Cofree Comonad definition and instances

--- src/CofreeX.hs
+++ src/CofreeX.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE TypeOperators, TypeApplications, RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
 
-module FreeX () where
+module CofreeX () where
 
 class Functor w => Comonad w where
   extract :: w a -> a
@@ -44,3 +44,49 @@ instance Comonad (Store s) where
   extract (Store f s) = f s
   duplicate (Store f s) = Store (Store f) s
 
+-- Define Cofree comonad
+data Cofree f a = Cofree a (f (Cofree f a))
+
+instance Functor f => Functor (Cofree f) where
+  fmap f (Cofree a fcf) = Cofree (f a) (fmap (fmap f) fcf)
+
+instance Functor f => Comonad (Cofree f) where
+  extract (Cofree a fcf) = a
+  duplicate cof@(Cofree a fcf) = Cofree cof (fmap duplicate fcf)
+
+-- F-Alg and Cofree Comonad
+type Coalg f a = a -> f a
+
+newtype Fix f = In { out :: f (Fix f) }
+instance Show (f (Fix f)) => Show (Fix f) where
+  show (In fix) = "(In (" ++ show fix ++ "))"
+
+ana :: Functor f => Coalg f a -> a -> Fix f
+ana coalg = In . fmap (ana coalg) . coalg
+
+-- Cofree comonoid is Stream a
+type CofreeComon a = Stream a
+
+-- data Stream a = Cons a (Stream a)
+--               deriving Show
+
+data StreamF a x = SConsF a x
+                 deriving Show
+
+instance Functor (StreamF a) where
+  fmap f (SConsF a x) = SConsF a (f x)
+
+data ListF a x = NilF
+               | ConsF a x
+
+instance Functor (ListF a) where
+  fmap f NilF = NilF
+  fmap f (ConsF a x) = ConsF a (f x)
+
+unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+unfoldr coalg b = case coalg b of
+  Nothing -> []
+  Just (a, b') -> a : unfoldr coalg b'
+
+genNat = unfoldr go 0
+  where go b = Just (b, b + 1)
