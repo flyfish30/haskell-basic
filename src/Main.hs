0c761c01916657cadba6bd325e9c06cde219c55d
Parker Liu <liuchangsheng30@gmail.com>
2020-05-10 18:43:47 +0800

    Add slide window over group

--- src/Main.hs
+++ src/Main.hs
@@ -13,9 +13,13 @@ main = do
   -- putStrLn $ show $ last (slideWindowSimple 5 sum xs :: [Int])
   -- putStrLn $ show $ last (slideWindowAccuR 5 (+) 0 xs :: [Int])
   -- putStrLn $ show $ last (slideWindowSemigrp 5 xs :: [Int])
-  putStrLn $ show $ V.last (slideWindowVSimple 5 V.sum $ V.fromList xs :: V.Vector Int)
+  -- putStrLn $ show $ last (slideWindowGroup 5 xs :: [Int])
+  -- putStrLn $ show $ last (slideWindowInt 5 xs :: [Int])
+  -- putStrLn $ show $ V.last (slideWindowVSimple 5 V.sum $ V.fromList xs :: V.Vector Int)
   -- putStrLn $ show $ V.last (slideWindowVAccuR 5 (+) 0 $ V.fromList xs :: V.Vector Int)
   -- putStrLn $ show $ V.last (slideWindowVSemigrp 5 $ V.fromList xs :: V.Vector Int)
+  -- putStrLn $ show $ V.last (slideWindowVGroup 5 $ V.fromList xs :: V.Vector Int)
+  putStrLn $ show $ V.last (slideWindowVInt 5 $ V.fromList xs :: V.Vector Int)
   -- img <- readImage "data/yingwuhua.jpg"
   -- writePng "output.png" . unfocus . (=>> medianImage) . focus $ img
   -- writePng "output.png" . unfocus . (=>> blurImage) . focus $ img
--- src/SlideWindow.hs
+++ src/SlideWindow.hs
@@ -3,6 +3,8 @@
 module SlideWindow where
 
 import           Data.List
+import           Data.Foldable
+import           Data.Group
 import qualified Data.Vector.Unboxed           as V
 import qualified Data.Vector.Unboxed.Mutable   as MV
 import           Control.Monad.ST
@@ -29,6 +31,10 @@ instance Monoid Int where
   mempty  = 0
   mappend = (<>)
 
+instance Group Int where
+  invert = negate
+  pow x n = x * fromIntegral n
+
 slideWindowSemigrp :: Semigroup a => Int -> [a] -> [a]
 slideWindowSemigrp n xs
   | length xs < n = []
@@ -36,8 +42,37 @@ slideWindowSemigrp n xs
   where go []     (b:bs) = [b]
         go (t:ts) (b:bs) = b : go ts (map (<> t) bs ++ [t])
 
+slideWindowGroup :: Group a => Int -> [a] -> [a]
+slideWindowGroup n xs
+  | length xs < n = []
+  | otherwise     = go (length xs) xs $ foldr1 (<>) (take n xs)
+  where go l (t:ts) b
+          | l < n + 1 = [b]
+          | otherwise = b : go (l-1) ts (b <> invert t <> ts !! (n - 1))
+
+slideWindowInt :: Int -> [Int] -> [Int]
+slideWindowInt n xs
+  | length xs < n = []
+  | otherwise     = go (length xs) xs $ sum (take n xs)
+  where go l (t:ts) b
+          | l < n + 1 = [b]
+          | otherwise = b : go (l-1) ts (b - t + ts !! (n - 1))
+
 -- Slide window on vector
 --
+mapModify f vec = go (MV.length vec - 1)
+  where go 0 = MV.modify vec f 0
+        go n = do MV.modify vec f n
+                  go (n - 1)
+
+shiftMapInAtN f vec = go 0 (MV.length vec - 2)
+  where go n l
+          | n < l     = do a <- MV.read vec (n + 1)
+                           MV.write vec n $ f a
+                           go (n + 1) l
+          | otherwise = do a <- MV.read vec (n + 1)
+                           MV.write vec n $ f a
+
 slideWindowVSimple :: (MV.Unbox a, MV.Unbox b)
                    => Int -> (V.Vector a -> b) -> V.Vector a -> V.Vector b
 slideWindowVSimple n f vec
@@ -71,13 +106,48 @@ slideWindowVSemigrp :: (Semigroup a, MV.Unbox a)
 slideWindowVSemigrp n vec
   | V.length vec < n = V.empty
   | otherwise        = runST $ do mvec <- (MV.new (V.length vec - n + 1))
-                                  go 0 mvec (V.drop n vec) (V.scanr1 (<>) $ V.take n vec)
+                                  bvec <- V.thaw $ V.scanr1 (<>) $ V.take n vec
+                                  go 0 mvec (V.drop n vec) bvec
   where go i mvec tvec bvec
           | V.null tvec = do
+                        b <- MV.read bvec 0
                         MV.write mvec i b
                         V.unsafeFreeze mvec
           | otherwise   = do
+                        b <- MV.read bvec 0
                         MV.write mvec i b
-                        go (i+1) mvec ts $ V.map (<> t) bs `V.snoc` t
+                        shiftMapInAtN (<> t) bvec
+                        MV.write bvec (MV.length bvec - 1) t
+                        go (i+1) mvec ts bvec
           where (t, ts) = (V.head tvec, V.tail tvec)
-                (b, bs) = (V.head bvec, V.tail bvec)
+
+slideWindowVGroup :: (Group a, MV.Unbox a)
+                    => Int -> V.Vector a -> V.Vector a
+slideWindowVGroup n vec
+  | len < n   = V.empty
+  | otherwise = runST $ do mvec <- (MV.new (len - n + 1))
+                           go 0 mvec vec (V.foldr1 (<>) $ V.take n vec)
+  where len = V.length vec
+        go i mvec tvec b
+          | i < len - n = do
+                        MV.write mvec i b
+                        go (i+1) mvec ts (b <> invert t <> ts V.! (n - 1))
+          | otherwise   = do
+                        MV.write mvec i b
+                        V.unsafeFreeze mvec
+          where (t, ts) = (V.head tvec, V.tail tvec)
+
+slideWindowVInt :: Int -> V.Vector Int -> V.Vector Int
+slideWindowVInt n vec
+  | len < n   = V.empty
+  | otherwise = runST $ do mvec <- (MV.new (len - n + 1))
+                           go 0 mvec vec (V.foldr1 (+) $ V.take n vec)
+  where len = V.length vec
+        go i mvec tvec b
+          | i < len - n = do
+                        MV.write mvec i b
+                        go (i+1) mvec ts (b - t + ts V.! (n - 1))
+          | otherwise   = do
+                        MV.write mvec i b
+                        V.unsafeFreeze mvec
+          where (t, ts) = (V.head tvec, V.tail tvec)
