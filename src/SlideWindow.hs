7f4a82f2c1b87b38e8770231aa104b3ac5580329
Parker Liu <liuchangsheng30@gmail.com>
2020-05-11 17:39:46 +0800

    Add bang pattern in parameters for speedup performance

--- src/SlideWindow.hs
+++ src/SlideWindow.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE BangPatterns #-}
 
 module SlideWindow where
 
@@ -13,7 +14,7 @@ slideWindowSimple :: Int -> ([a] -> b) -> [a] -> [b]
 slideWindowSimple n f xs
   | length xs < n = []
   | otherwise     = go (length xs) xs
-  where go l ts
+  where go !l !ts
           | l < n     = []
           | otherwise = f (take n ts) : go (l-1) (tail ts)
 
@@ -46,7 +47,7 @@ slideWindowGroup :: Group a => Int -> [a] -> [a]
 slideWindowGroup n xs
   | length xs < n = []
   | otherwise     = go (length xs) xs $ foldr1 (<>) (take n xs)
-  where go l (t:ts) b
+  where go !l (t:ts) !b
           | l < n + 1 = [b]
           | otherwise = b : go (l-1) ts (b <> invert t <> ts !! (n - 1))
 
@@ -54,7 +55,7 @@ slideWindowInt :: Int -> [Int] -> [Int]
 slideWindowInt n xs
   | length xs < n = []
   | otherwise     = go (length xs) xs $ sum (take n xs)
-  where go l (t:ts) b
+  where go !l (t:ts) !b
           | l < n + 1 = [b]
           | otherwise = b : go (l-1) ts (b - t + ts !! (n - 1))
 
@@ -75,11 +76,12 @@ shiftMapInAtN f vec = go 0 (MV.length vec - 2)
 
 slideWindowVSimple :: (MV.Unbox a, MV.Unbox b)
                    => Int -> (V.Vector a -> b) -> V.Vector a -> V.Vector b
+-- slideWindowVSimple :: Int -> (V.Vector Int -> Int) -> V.Vector Int -> V.Vector Int
 slideWindowVSimple n f vec
   | V.length vec < n = V.empty
   | otherwise        = runST $ do mvec <- (MV.new (V.length vec - n + 1))
                                   go 0 mvec vec
-  where go i mvec tvec
+  where go !i mvec !tvec
           | V.length tvec < n = V.unsafeFreeze mvec
           | otherwise         = do
                               MV.write mvec i $ f (V.take n tvec)
@@ -91,7 +93,7 @@ slideWindowVAccuR n f z vec
   | V.length vec < n = V.empty
   | otherwise        = runST $ do mvec <- (MV.new (V.length vec - n + 1))
                                   go 0 mvec (V.drop n vec) (V.scanr f z $ V.take n vec)
-  where go i mvec tvec bvec
+  where go !i mvec !tvec !bvec
           | V.null tvec = do
                         MV.write mvec i b
                         V.unsafeFreeze mvec
@@ -103,12 +105,13 @@ slideWindowVAccuR n f z vec
 
 slideWindowVSemigrp :: (Semigroup a, MV.Unbox a)
                     => Int -> V.Vector a -> V.Vector a
+-- slideWindowVSemigrp :: Int -> V.Vector Int -> V.Vector Int
 slideWindowVSemigrp n vec
   | V.length vec < n = V.empty
   | otherwise        = runST $ do mvec <- (MV.new (V.length vec - n + 1))
                                   bvec <- V.thaw $ V.scanr1 (<>) $ V.take n vec
                                   go 0 mvec (V.drop n vec) bvec
-  where go i mvec tvec bvec
+  where go !i mvec !tvec !bvec
           | V.null tvec = do
                         b <- MV.read bvec 0
                         MV.write mvec i b
@@ -122,13 +125,14 @@ slideWindowVSemigrp n vec
           where (t, ts) = (V.head tvec, V.tail tvec)
 
 slideWindowVGroup :: (Group a, MV.Unbox a)
-                    => Int -> V.Vector a -> V.Vector a
+                  => Int -> V.Vector a -> V.Vector a
+-- slideWindowVGroup :: Int -> V.Vector Int -> V.Vector Int
 slideWindowVGroup n vec
   | len < n   = V.empty
   | otherwise = runST $ do mvec <- (MV.new (len - n + 1))
                            go 0 mvec vec (V.foldr1 (<>) $ V.take n vec)
   where len = V.length vec
-        go i mvec tvec b
+        go !i mvec !tvec !b
           | i < len - n = do
                         MV.write mvec i b
                         go (i+1) mvec ts (b <> invert t <> ts V.! (n - 1))
@@ -137,13 +141,14 @@ slideWindowVGroup n vec
                         V.unsafeFreeze mvec
           where (t, ts) = (V.head tvec, V.tail tvec)
 
+-- slideWindowVInt :: (MV.Unbox a, Num a) => Int -> V.Vector a -> V.Vector a
 slideWindowVInt :: Int -> V.Vector Int -> V.Vector Int
 slideWindowVInt n vec
   | len < n   = V.empty
   | otherwise = runST $ do mvec <- (MV.new (len - n + 1))
                            go 0 mvec vec (V.foldr1 (+) $ V.take n vec)
   where len = V.length vec
-        go i mvec tvec b
+        go !i mvec !tvec !b
           | i < len - n = do
                         MV.write mvec i b
                         go (i+1) mvec ts (b - t + ts V.! (n - 1))
